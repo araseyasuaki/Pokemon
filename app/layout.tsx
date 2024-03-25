31aae5ae632ac6ec2f2b01c64ec843c6e7744657
Your Name <your.email@example.com>
2024-03-25 17:56:48 +0900

    メニューバー追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Menu } from "@/app/parts/menu";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={inter.className}>
+        <Menu/>
         {children}
       </body>
     </html>
