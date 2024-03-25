52551d8d244958525b58793a6297da599e975a6f
Your Name <your.email@example.com>
2024-03-25 17:05:52 +0900

    テスト３

--- app/Parts/MenuLink.tsx
+++ app/Parts/MenuLink.tsx
@@ -1,5 +1,6 @@
+'use client'
+
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import clsx from "clsx";
 
 interface MenuBerProps {
@@ -9,10 +10,8 @@ interface MenuBerProps {
 
 export function MenuBer(props: MenuBerProps) {
 
-  const pathname = usePathname();
-
   return(
-    <Link className={clsx("py-4 px-6 relative font-bold hover:bg-blue-100 hover:text-blue-700 transition duration-400 ease-out",{"a after:absolute after:w-2 after:h-2 after:rounded-xl after:bg-blue-700 after:left-2.5 after:top-1/2 after:-translate-y-1/2": pathname === `/${props.link}`})} href={props.link}>
+    <Link className="py-4 px-6 relative font-bold hover:bg-blue-100 hover:text-blue-700 transition duration-400 ease-out" href={props.link}>
       {props.text}
     </Link>
   )
