b1aa7d046db6434156724b29ce1bbb58c1ebd8d6
Your Name <your.email@example.com>
2024-03-25 17:39:26 +0900

    テスト６

--- app/Parts/Menu.tsx
+++ app/Parts/Menu.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import clsx from "clsx";
-import { MenuBer } from "@/app/parts/menuLink";
+import { MenuBer } from "@/app/parts/menulink";
   const areaData = [
     { link: "kantoo", text: "カントー地方" },
     { link: "zyouto", text: "ジョウト地方" },
