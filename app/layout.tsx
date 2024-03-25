8646f21c5c9613069cfa7c3429c53b68eb27c3f6
Your Name <your.email@example.com>
2024-03-25 17:27:44 +0900

    テスト５

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Menu } from "@/app/parts/menu";
+// import { Menu } from "@/app/parts/menu";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={inter.className}>
-      <Menu/>
+      {/* <Menu/> */}
         {children}
       </body>
     </html>
