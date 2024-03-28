0342c3998e396d414fea8bdefca8976a2efbb057
Your Name <your.email@example.com>
2024-03-28 10:11:28 +0900

    APIテスト２

--- app/(area)/kantoo/page.tsx
+++ app/(area)/kantoo/page.tsx
@@ -1,59 +1,126 @@
+// 'use client'
+
+// import { useState, useEffect } from "react";
+
+// export default function Kantoo() {
+
+//   interface resFlA {
+//     sprites: {
+//       other: {
+//         'officia-artwork': {
+//           front_default: string
+//         }
+//       }
+//     },
+//   }
+//   interface resFlB {
+//     names: string[],
+//   }
+
+//   const [ pokeDataA, setPokeDataA ] = useState([]);
+//   const [ pokeDataB, setPokeDataB ] = useState([]);
+
+//   useEffect(()=>{
+//     const fetchData = async () => {
+//       const resDataA = [];
+//       const resDataB = [];
+//       try{
+//         for( let i = 1; i <= 3; i ++){
+//           const resA = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+//           const resB = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${i}`);
+//           const dataA = await resA.json();
+//           const dataB = await resB.json();
+//           resDataA.push(dataA);
+//           resDataB.push(dataB);
+//         }
+//         setPokeDataA(resDataA)
+//         setPokeDataB(resDataB)
+//       }catch (error){
+//         console.error('Error fetching Pokemon data', error);
+//       }
+//     }
+//     fetchData();
+//   },[])
+
+//   return (
+//     <>
+
+//       <main className="flex flex-col text-left">
+//         {pokeDataA ? (
+//           <div>
+//             {pokeDataA.map((data, index)=>(
+//               <p>{data.name}</p>
+//             ))}
+//           </div>
+//         ) : (
+//           <p>aaa</p>
+//         )}
+//       </main>
+
+//     </>
+//   );
+// }
+
 'use client'
 
 import { useState, useEffect } from "react";
 
-// interface resFlA {
-//   sprites: { other: { 'official-artwork': { front_default: string}} },
-//   name: string,
-// }
-// interface resFlB {
-//   names: { name: string }[],
-// }
-
 export default function Kantoo() {
 
-  const [ pokeDataA, setPokeDataA ] = useState(['']);
-  const [ pokeDataB, setPokeDataB ] = useState(['']);
-  let resData: string[] = [];
-
-  useEffect(()=>{
-    const fetchData = async () => {
-      try{
-        for( let i = 1; i <= 3; i ++){
-          const resa = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
-          const resb = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${i}`);
-          const dataA = await resa.json();
-          const dataB = await resb.json();
-          resData.push(dataA.sprites.other["official-artwork"].front_default);
-          console.log(resData[3])
+  interface resFlA {
+    sprites: {
+      other: {
+        'official-artwork': {
+          front_default: string
         }
-      }catch (error){
+      }
+    },
+    name: string
+  }
+  interface resFlB {
+    names: string,
+  }
+
+  const [pokeDataA, setPokeDataA] = useState<resFlA[]>([]);
+  const [pokeDataB, setPokeDataB] = useState<resFlB[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const resDataA: resFlA[] = [];
+        const resDataB: resFlB[] = [];
+        for (let i = 152; i <= 251; i++) {
+          const resA = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+          const resB = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${i}`);
+          const dataA: resFlA = await resA.json();
+          const dataB: resFlB = await resB.json();
+          resDataA.push(dataA);
+          resDataB.push(dataB);
+        }
+        setPokeDataA(resDataA);
+        setPokeDataB(resDataB);
+      } catch (error) {
         console.error('Error fetching Pokemon data', error);
       }
     }
     fetchData();
-    setPokeDataA(resData);
-  },[])
+  }, [])
 
   return (
     <>
 
       <main className="flex flex-col text-left">
-        {/* {pokeDataA ? (
-          <img src={pokeDataA.sprites.other["official-artwork"].front_default} width={180} height={180} alt={pokeDataA.name}/>
+        {pokeDataA.length > 0 ? (
+          <div>
+            {pokeDataA.map((data, index) => (
+              <p key={index}>{data.name}</p>
+            ))}
+          </div>
         ) : (
-          <p>aaa</p>
+          <p>Loading...</p>
         )}
-        {pokeDataB ? (
-          <p>{pokeDataB.names[0].name}</p>
-        ) : (
-          <p>aaa</p>
-        )} */}
-        a
-        {/* <img src={setPokeDataA[0]} alt="a" /> */}
       </main>
 
     </>
   );
 }
-
